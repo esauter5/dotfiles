0b3ff18222a7fb8b5babc731b2b586742b4c03c8
Paul Ostazeski <paul.ostazeski@gmail.com>
2015-01-05 16:29:10 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -ex
-curl -L http://install.ohmyz.sh
-#yes | sudo pacman -S tmux
-sudo sed -i "/esauter/ s/bash/zsh/" /etc/passwd
-./init.sh
+yes | sudo pacman -S zsh
+curl -L http://install.ohmyz.sh | sh
+zsh ./init.sh
